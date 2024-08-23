b9ad789a5e2b42b7431dc22c5e1cf921ed7dfad0
AmosAP <nekumbi99@gmail.com>
2024-08-24 00:26:33 +0100

    Atualizacao Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/navbar";
 
+
 export default function App() {
   return (
     <>
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,14 +1,23 @@
 import "./style.css";
+import logo_light from '../../assets/logo-black.png'
+import logo_dark from '../../assets/logo-white.png'
+import Icon_pesquisa_light from '../../assets/search-w.png'
+import Icon_pesquisa_dark from '../../assets/search-b.png'
+import toogle_light from '../../assets/night.png'
+import toogle_dark from '../../assets/day.png'
+import logo_abc from '../../assets/Designer.jpeg'
+
+
 
 export default function Navbar() {
   return (
     <nav className="navbar-container">
       <div className="navbar">
-        <div className="navbar-logo">logo</div>
-        <img src="" alt=""></img>
+      <img src={logo_light} alt="" className="Icon"/>
+       {/*<div className="navbar-logo">logo</div>*/}
         <ul className="navbar-links">
           <li>
-            <a href="#Nekumbi">AMOS</a>
+            <a href="#Nekumbi">NEKUMBI</a>
           </li>
           <li>
             <a href="#Manuel">MANUEL</a>
@@ -17,6 +26,11 @@ export default function Navbar() {
             <a href="#React">EVORA</a>
           </li>
         </ul>
+        <div className="caixa_pesquisa">
+          <input type="text" placeholder="Pesquisa"/>
+          <img src={Icon_pesquisa_light} alt=""/>
+        </div>
+        <img src={toogle_light} alt="" className="togle-icon"/>
       </div>
     </nav>
   );
